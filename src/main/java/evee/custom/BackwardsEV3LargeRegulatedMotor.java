3ba52f026a2b71b2c6af34beadc25a95132ea474
Giacomo Scaparrotti <jack@pc-di-jack>
2025-02-15 17:53:36 +0100

    first commit

---
+++ src/main/java/evee/custom/BackwardsEV3LargeRegulatedMotor.java
@@ -0,0 +1,25 @@
+package evee.custom;
+
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+
+public class BackwardsEV3LargeRegulatedMotor extends EV3LargeRegulatedMotor {
+    /**
+     * Constructor
+     *
+     * @param motorPort motor port
+     */
+    public BackwardsEV3LargeRegulatedMotor(Port motorPort) {
+        super(motorPort);
+    }
+
+    @Override
+    public void forward() {
+        super.backward();
+    }
+
+    @Override
+    public void backward() {
+        super.forward();
+    }
+}
