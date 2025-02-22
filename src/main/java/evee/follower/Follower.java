0c5e31203f5d90c1519f0984b8aa8e930aee6765
Giacomo Scaparrotti <gscaparrotti@gmail.com>
2025-02-22 15:43:09 +0100

    Implement FSM for line following; add debug mode

--- src/main/java/evee/follower/Follower.java
+++ src/main/java/evee/follower/Follower.java
@@ -1,5 +1,6 @@
 package evee.follower;
 
+import ev3dev.actuators.ev3.EV3Led;
 import ev3dev.sensors.ev3.EV3ColorSensor;
 import evee.basicMovements.BasicMovements;
 import lejos.hardware.port.SensorPort;
@@ -7,28 +8,29 @@ import lejos.robotics.Color;
 import lejos.robotics.filter.MedianFilter;
 import lejos.robotics.subsumption.Behavior;
 import lombok.AllArgsConstructor;
-import lombok.Value;
+import org.jeasy.states.api.*;
+import org.jeasy.states.core.FiniteStateMachineBuilder;
+import org.jeasy.states.core.TransitionBuilder;
 
 import java.util.Random;
+import java.util.Set;
 
-import static evee.follower.Follower.Rotation.RotationKind.LEFT;
-import static evee.follower.Follower.Rotation.RotationKind.RIGHT;
+import static evee.follower.Follower.RotationKind.LEFT;
+import static evee.follower.Follower.RotationKind.RIGHT;
 
 public class Follower implements Behavior {
-
-    final Random rand = new Random();
 
     final BasicMovements basicMovements;
     final EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
 
     final MedianFilter filter = new MedianFilter(colorSensor, 3);
-    long lastBlackFoundTimestamp = -1;
-    long lastChangeTimestamp = -1;
 
-    Rotation currentRotation = new Rotation(LEFT);
+    final FollowerStateMachine followerStateMachine;
 
     public Follower(BasicMovements basicMovements) {
+        this.colorSensor.setFloodlight(Color.WHITE);
         this.basicMovements = basicMovements;
+        this.followerStateMachine = new FollowerStateMachine(this.basicMovements);
     }
 
     @Override
@@ -48,43 +50,20 @@ public class Follower implements Behavior {
     }
 
     public void updateRotation() {
+        final var colorID = getColorID();
+        try {
+            this.followerStateMachine.update(colorID);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int getColorID() {
         final float[] buffer = new float[1];
         filter.fetchSample(buffer, 0);
         final var colorID = (int) buffer[0];
         logColorID(colorID);
-        if (colorID == Color.BLACK) {
-            basicMovements.rotateToAngle(LEFT.angle);
-            this.currentRotation = new Rotation(LEFT);
-            this.lastBlackFoundTimestamp = System.currentTimeMillis();
-        } else {
-            if (System.currentTimeMillis() - lastBlackFoundTimestamp < 2000) {
-                basicMovements.rotateToAngle(RIGHT.angle);
-            } else if (System.currentTimeMillis() - lastChangeTimestamp > 2000) {
-                basicMovements.rotateToAngle(0);
-                if (System.currentTimeMillis() - lastBlackFoundTimestamp < 10000) {
-                    basicMovements.backOff();
-                }
-                final var randomAngle = rand.nextInt(15);
-                final var sign = rand.nextBoolean() ? 1 : -1;
-                System.out.println("New random angle: " + randomAngle + ", sign: " + sign);
-                basicMovements.rotateToAngle(randomAngle * sign);
-                basicMovements.forward();
-                this.lastChangeTimestamp = System.currentTimeMillis();
-            }
-            this.currentRotation = new Rotation(RIGHT);
-        }
-    }
-
-    @Value
-    static class Rotation {
-        RotationKind rotation;
-
-        @AllArgsConstructor
-        enum RotationKind {
-            LEFT(-20), RIGHT(20);
-            final int angle;
-        }
-
+        return colorID;
     }
 
     private static void logColorID(int colorID) {
@@ -114,6 +93,133 @@ public class Follower implements Behavior {
                 System.out.println("BROWN detected");
                 break;
         }
+    }
+
+    @AllArgsConstructor
+    enum RotationKind {
+        LEFT(-15), RIGHT(15);
+        final int angle;
+    }
+
+    private static class FollowerStateMachine {
+
+        private static final Random RANDOM = new Random();
+
+        static {
+            RANDOM.nextInt();
+        }
+
+        private static class BlackDetectedEvent extends AbstractEvent { }
+        private static class NotBlackDetectedEvent extends AbstractEvent { }
+        private static class BlackDetectionTooOldEvent extends AbstractEvent { }
+
+        private static final State BLACK_NOT_FOUND = new State("BLACK_NOT_FOUND");
+        private static final State BLACK_FOUND = new State("BLACK_FOUND");
+        private static final State BLACK_LOST = new State("BLACK_LOST");
+
+        private static final Set<State> STATES = Set.of(
+            BLACK_NOT_FOUND,
+            BLACK_FOUND,
+            BLACK_LOST
+        );
+
+        private BasicMovements basicMovements;
+        private final EV3Led led = new EV3Led(EV3Led.Direction.LEFT);
+        private long lastNotBlackFoundTimestamp = -1;
+
+        private final Transition BLACK_DETECTED_TRANSITION = new TransitionBuilder()
+            .name("BLACK_DETECTED")
+            .sourceState(BLACK_NOT_FOUND)
+            .targetState(BLACK_FOUND)
+            .eventType(BlackDetectedEvent.class)
+            .eventHandler((event) -> {
+                basicMovements.rotateToAngle(LEFT.angle);
+                led.setPattern(3);
+                System.out.println("BLACK_DETECTED");
+            })
+            .build();
+        private final Transition BLACK_DETECTED_AGAIN_TRANSITION = new TransitionBuilder()
+            .name("BLACK_DETECTED_AGAIN_TRANSITION")
+            .sourceState(BLACK_LOST)
+            .targetState(BLACK_FOUND)
+            .eventType(BlackDetectedEvent.class)
+            .eventHandler((event) -> {
+                basicMovements.rotateToAngle(LEFT.angle);
+                led.setPattern(3);
+                System.out.println("BLACK_DETECTED_AGAIN_TRANSITION");
+            })
+            .build();
+        private final Transition NOT_BLACK_DETECTED_TRANSITION = new TransitionBuilder()
+            .name("NOT_BLACK_DETECTED")
+            .sourceState(BLACK_FOUND)
+            .targetState(BLACK_LOST)
+            .eventType(NotBlackDetectedEvent.class)
+            .eventHandler((event -> {
+                basicMovements.rotateToAngle(RIGHT.angle);
+                led.setPattern(2);
+                lastNotBlackFoundTimestamp = event.getTimestamp();
+                System.out.println("NOT_BLACK_DETECTED");
+            }))
+            .build();
+        private final Transition BLACK_DETECTION_TOO_OLD_TRANSITION = new TransitionBuilder()
+            .name("BLACK_DETECTION_TOO_OLD")
+            .sourceState(BLACK_LOST)
+            .targetState(BLACK_NOT_FOUND)
+            .eventType(BlackDetectionTooOldEvent.class)
+            .eventHandler((event -> {
+                basicMovements.rotateToAngle(0);
+                basicMovements.backOff();
+                basicMovements.forward();
+                led.setPattern(0);
+                System.out.println("BLACK_DETECTION_TOO_OLD");
+                lastNotBlackFoundTimestamp = event.getTimestamp();
+            }))
+            .build();
+
+        private final Transition BLACK_DETECTION_STILL_TOO_OLD_TRANSITION = new TransitionBuilder()
+            .name("BLACK_DETECTION_TOO_OLD")
+            .sourceState(BLACK_NOT_FOUND)
+            .targetState(BLACK_NOT_FOUND)
+            .eventType(BlackDetectionTooOldEvent.class)
+            .eventHandler((event -> {
+                final var randomAngle = RANDOM.nextInt(15);
+                final var sign = RANDOM.nextBoolean() ? 1 : -1;
+                System.out.println("New random angle: " + randomAngle + ", sign: " + sign);
+                basicMovements.rotateToAngle(randomAngle * sign);
+                led.setPattern(0);
+                System.out.println("BLACK_DETECTION_TOO_OLD");
+                lastNotBlackFoundTimestamp = event.getTimestamp();
+            }))
+            .build();
+
+        private final Set<Transition> TRANSITIONS = Set.of(
+            BLACK_DETECTED_TRANSITION,
+            BLACK_DETECTED_AGAIN_TRANSITION,
+            NOT_BLACK_DETECTED_TRANSITION,
+            BLACK_DETECTION_TOO_OLD_TRANSITION,
+            BLACK_DETECTION_STILL_TOO_OLD_TRANSITION
+        );
+
+        private final FiniteStateMachine fsm = new FiniteStateMachineBuilder(STATES, BLACK_NOT_FOUND)
+            .registerTransitions(TRANSITIONS)
+            .build();
+
+        private FollowerStateMachine(final BasicMovements basicMovements) {
+            this.basicMovements = basicMovements;
+        }
+
+        public void update(final int colorID) throws FiniteStateMachineException {
+            if (colorID == Color.BLACK) {
+                fsm.fire(new BlackDetectedEvent());
+            } else {
+                fsm.fire(new NotBlackDetectedEvent());
+                if (System.currentTimeMillis() - lastNotBlackFoundTimestamp > 2000) {
+                    fsm.fire(new BlackDetectionTooOldEvent());
+                }
+
+            }
+        }
+
     }
 
 }
